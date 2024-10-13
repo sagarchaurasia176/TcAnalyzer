adf7cd7acb52eea7610e15da166ad4ea8384bde6
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-10-13 18:04:38 +0530

    mino change in the ui

--- tcanalyzer/src/page/FeaturePage.jsx
+++ tcanalyzer/src/page/FeaturePage.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const FeaturePage = ({ data }) => {
   return (
-    <div className="container mx-auto p-3 ">
-   
-    </div>
+    <section className=" p-3  flex float-left ml-1 space-x-4  bg-red-300">
+        {/* card */}
+      <div className=" bg-white p-3  h-[32vh] m-auto w-[100vw]  md:w-[20vw] sm:w-[12vw] rounded-lg ">
+        <div className=" text-black m-auto">
+          <span className=" font-header-font text-center ">{data.head}</span>
+          <div>
+            {data.icons}
+          </div>
+          <div>
+            <p>{data.para}</p>
+          </div>
+         
+        </div>
+      </div>
+    </section>
   );
 };
 
